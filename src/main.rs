96798d2420b5578059547cc5af5cfcbf250964e4
Andrew Rodriguez <andrewrod003@gmail.com>
2024-08-22 10:07:34 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
 use std::env;
 use std::fs;
 use std::io::{self, Write};
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
         return;
     }
-
     let command = &args[1];
     let filename = &args[2];
-
     match command.as_str() {
         "tokenize" => {
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
 
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
-            }
+            tokenize(&file_contents);
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
             return;
         }
     }
+    fn tokenize(input: &str) {
+        for char in input.chars() {
+            match char {
+                '(' => println!("LEFT_PAREN ( null"),
+                ')' => println!("RIGHT_PAREN ) null"),
+                _ => {} // Ignore other characters for now
+            }
+        }
+        println!("EOF  null");
+    }
 }
+
